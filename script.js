001e5d62053b9c9be35b6332bee7b2bbcf8cb079
Filitov Mikhail <filitovme@gmail.com>
2023-12-23 11:49:41 +0000

    fsdfsdf

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const achievementsContainer = document.getElementByClass('achievementsContainer');
+    const achievementsContainer = document.getElementsByClassName('achievementsContainer');
 
-    fetch('achievements.json')
+    fetch('achievements.json') 
         .then(response => response.json())
         .then(data => {
             data.forEach(achievementData => {
